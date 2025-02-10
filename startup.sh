f0f0259e31d3a7fe0ac0882348a2f36751616cd1
danhamilton-epuki <92871198+danhamilton-epuki@users.noreply.github.com>
2025-02-10 20:47:17 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 apt-get update
-apt-get install -y unixodbc-dev gcc g++ build-essential libssl-dev
+apt-get install -y --no-install-recommends \
+    build-essential \
+    unixodbc \
+    unixodbc-dev \
+    libssl-dev \
+    libffi-dev \
+    gcc g++
+
 gunicorn -w 4 -b 0.0.0.0:8000 app:app
